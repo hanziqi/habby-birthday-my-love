b08c94bf3e01b79d561e864ceea6026fc017c7a9
zqhan <zqhan@ctrip.com>
2019-09-26 01:58:16 +0800

    Updates

---
+++ precache-manifest.b542f7f5bc5d7b3fddbaa48cc5527afa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe8ce3c42a6aea4a32b1ea79055be5df",
+    "url": "/habby-birthday-my-love/static/media/15.fe8ce3c4.jpeg"
+  },
+  {
+    "revision": "47204e63ec19dfb87a55",
+    "url": "/habby-birthday-my-love/static/css/main.d0dce5ea.chunk.css"
+  },
+  {
+    "revision": "69b40776cb4646b65802",
+    "url": "/habby-birthday-my-love/static/js/1.69b40776.chunk.js"
+  },
+  {
+    "revision": "456493abe623484c1eb0",
+    "url": "/habby-birthday-my-love/static/js/runtime~main.456493ab.js"
+  },
+  {
+    "revision": "d3457c31d463c4acf6cac4a59bfa5b57",
+    "url": "/habby-birthday-my-love/static/media/1.d3457c31.jpeg"
+  },
+  {
+    "revision": "9b24e5b88ef1537dd78ef9ffdc49d3f7",
+    "url": "/habby-birthday-my-love/static/media/10.9b24e5b8.jpeg"
+  },
+  {
+    "revision": "8d42fd8579de7db5889d803fd3652322",
+    "url": "/habby-birthday-my-love/static/media/11.8d42fd85.jpeg"
+  },
+  {
+    "revision": "9f0630ccf6ed32133ac13a05fb05c80a",
+    "url": "/habby-birthday-my-love/static/media/12.9f0630cc.jpeg"
+  },
+  {
+    "revision": "b5562fbfa9bc393ae9d6424de792aee7",
+    "url": "/habby-birthday-my-love/static/media/13.b5562fbf.jpeg"
+  },
+  {
+    "revision": "4eaf48964aca9f98b8816eee8ac42a58",
+    "url": "/habby-birthday-my-love/static/media/14.4eaf4896.jpeg"
+  },
+  {
+    "revision": "47204e63ec19dfb87a55",
+    "url": "/habby-birthday-my-love/static/js/main.47204e63.chunk.js"
+  },
+  {
+    "revision": "2da6b033351a4a417829e9a841287c4a",
+    "url": "/habby-birthday-my-love/static/media/16.2da6b033.jpeg"
+  },
+  {
+    "revision": "cdd7c2eb7f36aff9e167a6092dbf8ac6",
+    "url": "/habby-birthday-my-love/static/media/2.cdd7c2eb.jpeg"
+  },
+  {
+    "revision": "a3c902592b354d70673c1bee7f152cdb",
+    "url": "/habby-birthday-my-love/static/media/3.a3c90259.jpeg"
+  },
+  {
+    "revision": "8cc8eee94124cff45d51e7fd110330d9",
+    "url": "/habby-birthday-my-love/static/media/4.8cc8eee9.jpeg"
+  },
+  {
+    "revision": "fdcbf22fbb3192c9e1040e966315780a",
+    "url": "/habby-birthday-my-love/static/media/5.fdcbf22f.jpeg"
+  },
+  {
+    "revision": "da2c8a1233741214899c7f9be8562c35",
+    "url": "/habby-birthday-my-love/static/media/6.da2c8a12.jpeg"
+  },
+  {
+    "revision": "7c1991399682cd7c214b0a98c69e6cd6",
+    "url": "/habby-birthday-my-love/static/media/7.7c199139.jpeg"
+  },
+  {
+    "revision": "85b3db9e890b3ba44986e209ab1d3b18",
+    "url": "/habby-birthday-my-love/static/media/8.85b3db9e.jpeg"
+  },
+  {
+    "revision": "2e3e36446aee84062f3fdc4a068e2951",
+    "url": "/habby-birthday-my-love/static/media/9.2e3e3644.jpeg"
+  },
+  {
+    "revision": "f127d552eace12dba9bfc0c8a265ea8f",
+    "url": "/habby-birthday-my-love/index.html"
+  }
+];
